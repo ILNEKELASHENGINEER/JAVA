d1ff2bc4c69655c76a5952e5c0ca9eed292aab09
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-10 01:09:14 +0530

    Update Class_with_attributes.java

--- Class_with_attributes.java
+++ Class_with_attributes.java
@@ -58,7 +58,6 @@ class Main{
       System.out.println((t+1)+" "+maxi);
 }
 }
-
 /*
 INPUT
 5
@@ -77,4 +76,90 @@ OUTPUT
 1 98
 1 96
 4 79
-1 259
+1 259 
+*/
+
+//ABOVE PROGRAM IS FOR BEGINNER FRIENDLY AND HIGHLY LOGICAL SYNTAX.
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    OR     ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+//BELOW PROGRAM IS SIMPLIFIED OF ABOVE PROGRAM AND TIME COMPLEXITY IS REDUCED IN BELOW PROGRAM.
+
+import java.io.*;
+import java.util.*;
+class Student{
+int rollno,m1,m2,m3,sum;
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        int n,r1=0,r2=0,r3=0,r4=0;
+        Scanner s=new Scanner(System.in);
+        n=s.nextInt();
+        Student[] a=new Student[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=new Student();
+            a[i].rollno=s.nextInt();
+            a[i].m1=s.nextInt();
+            a[i].m2=s.nextInt();
+            a[i].m3=s.nextInt();
+            a[i].sum=a[i].m1+a[i].m2+a[i].m3;
+            System.out.println(a[i].sum);
+        }
+        int hm1=a[0].m1;
+        int hm2=a[0].m2;
+        int hm3=a[0].m3;
+        int hs=a[0].m1+a[0].m2+a[0].m3;
+        for(int i=0;i<n;i++)
+        {
+            if(hm1<=a[i].m1)
+            {
+                hm1=a[i].m1;
+                r1=a[i].rollno;
+            }
+            if(hm2<=a[i].m2)
+            {
+                hm2=a[i].m2;
+                r2=a[i].rollno;
+            }
+            if(hm3<=a[i].m3)
+            {
+                hm3=a[i].m3;
+                r3=a[i].rollno;
+            }
+            if(hs<=a[i].m1+a[i].m2+a[i].m3)
+            {
+                hs=a[i].m1+a[i].m2+a[i].m3;
+                r4=a[i].rollno;
+            }
+        }
+        System.out.println(r1+" "+hm1);
+        System.out.println(r2+" "+hm2);
+        System.out.println(r3+" "+hm3);
+        System.out.println(r4+" "+hs);
+    }
+}
+/*
+INPUT
+5
+1 98 85 76
+2 85 74 65
+3 85 96 75
+4 52 65 79
+5 52 75 65
+
+OUTPUT
+259
+224
+256
+196
+192
+1 98
+1 96
+4 79
+1 259 
+*/
