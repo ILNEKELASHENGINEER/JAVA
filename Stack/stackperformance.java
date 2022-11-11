2bd3f0a3f3f57da11b47d8b3478ca3941b4372a7
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-11 11:21:44 +0530

    Stack performance in java using inbuilt class
    In this program
    push n elements into the stack.
    perform pop operation on the stack
    print the top element from the stack
    search for a particular element in the stack.
    HOLA HAPPY PROGRAMMING :)

---
+++ Stack/stackperformance.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+class Main{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int a,b,top = -1;
+        a=in.nextInt();
+        int arr[]=new int[a];
+        // a=in.nextInt();
+        for(int i=0;i<a;i++){
+            arr[i]=in.nextInt();
+        }
+        b = in.nextInt();
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0;i<a;i++){
+            top++;
+            stk.push(arr[i]);
+        }
+        System.out.println(stk.peek());
+        stk.pop();
+        System.out.println("Top Element:"+stk.peek());
+            if(stk.search(b)>0)
+            System.out.println("Element is found at position:"+stk.search(b));
+            else{
+                System.out.println("Element not found");
+            }
+        }
+    }
+/*
+5
+1
+2
+3
+4
+5
+4
+OUTPUT
+5
+Top Element:4
+Element is found at position:1
+
+5
+120
+240
+360
+480
+864
+218
+
+864
+Top element:480
+Element not found
+*/
