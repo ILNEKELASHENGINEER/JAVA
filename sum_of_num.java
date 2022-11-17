a0da74b25624af95c95747d8060ee92f17027f06
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-17 23:49:58 +0530

    JAVA program for summation of two number
    simple and only for beginner.
    Hola happy programming :)

---
+++ sum_of_num.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class main{
+public static void main(String []args){
+Scanner in = new Scanner(System.in);
+int a = in.nextInt();
+int b = in.nextInt();
+int c = a+b;
+System.out.println("Sum of "+a+"+"+"b"+" = "+c);
+}}
+
+/*
+INPUT
+52
+12
+OUTPUT
+Sum of 52+12 = 64
+*/
