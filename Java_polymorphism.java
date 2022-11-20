c4f6dd4dff063e760602a307294e35e08ada6de4
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-20 23:56:44 +0530

    Java Runtime Polymorphism Example: Shape

---
+++ Java_polymorphism.java
@@ -0,0 +1,28 @@
+class Shape{  
+void draw(){System.out.println("drawing...");}  
+}  
+class Rectangle extends Shape{  
+void draw(){System.out.println("drawing rectangle...");}  
+}  
+class Circle extends Shape{  
+void draw(){System.out.println("drawing circle...");}  
+}  
+class Triangle extends Shape{  
+void draw(){System.out.println("drawing triangle...");}  
+}  
+class TestPolymorphism2{  
+public static void main(String args[]){  
+Shape s;  
+s=new Rectangle();  
+s.draw();  
+s=new Circle();  
+s.draw();  
+s=new Triangle();  
+s.draw();  
+}  
+}  
+/*
+SBI Rate of Interest: 8.4
+ICICI Rate of Interest: 7.3
+AXIS Rate of Interest: 9.7
+*/
