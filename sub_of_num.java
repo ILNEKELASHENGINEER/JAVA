9538dcf30d3127f75e1a3ed2f6f77f847f0e16ce
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-11-29 23:34:22 +0530

    Subraction of two numbers in java
    This is very very basic beginner level program just for understanding.
    Hola happy programming :)

---
+++ sub_of_num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Main{
+public static void main(String []args){
+Scanner in = new Scanner(System.in);
+System.out.print("Enter the 1st number -> ");
+int a = in.nextInt();
+System.out.print("Enter the 2nd number -> ");
+int b = in.nextInt();
+int c = a-b;
+System.out.println("Difference between two given numbers are : "+c);
+}
+}
+
+/*
+INPUT
+Enter the 1st number -> 10
+Enter the 2nd number -> 5
+
+OUTPUT
+Difference between two given numbers are : 5
+*/
